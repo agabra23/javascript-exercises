b6f06d7fd2c1a4449ef3a55f8b100084df194361
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-10 18:41:50 -0500

    Do 06 exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+//Rules:
+//div by 4 == leap
+//div by 400 == leap
+//div by 100 == not leap
+//else == not leap
 
+const leapYears = function() {
+const year = arguments[0];
+    if (year % 400 === 0){return true}
+    else if (year % 100 === 0){return false}
+    else if (year % 4 === 0){return true}
+    return false;
 };
 
 // Do not edit below this line
