8da19f5191cec87bfc0bbd29334263f512eb416d
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-10 17:24:55 -0500

    Do 02 Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = str
+   
+    if(num === 0){return ''}
+    if (num < 0) {return 'ERROR'}
+    if (num === 1){return str;}
+    
+    for (let i = 2; i <= num; i++){
+        result += str;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
