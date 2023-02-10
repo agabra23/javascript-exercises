03ecab96be15ea1cfb6866d76d595ff671757615
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-10 18:49:37 -0500

    Do 07 Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function() {     // (x-32) * (5/9)
+  let farenheit = arguments[0];
+  let result = 0;
+  result = (farenheit-32) * (5/9);
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function() {      // x * (9/5) + 32
+  let celsius = arguments[0];
+  let result = celsius * (9/5) + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
