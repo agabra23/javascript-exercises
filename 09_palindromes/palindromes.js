0986d3bd0cce1ed31030e9b2724935bbd7fdb5d7
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-23 19:30:47 -0500

    Do 09 Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
 const palindromes = function () {
+    let target = arguments[0];
+    let str = target;
 
+    str = str.toLowerCase();
+    target = target.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    target = target.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g ,"");
+
+    str = str.replace(/\s/g, '');
+    target = target.replace(/\s/g, '');
+
+    let arr = str.split('')
+    arr.reverse();
+    str = arr.join('');
+    return (str === target)
 };
 
 // Do not edit below this line
