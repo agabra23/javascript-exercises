0272e2b9e71ae878c05150e7a2d0ac8e16341b10
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-23 19:15:41 -0500

    Do 08 Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	let arr = arguments[0];
+  return arr.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
 const multiply = function() {
-
+  let arr = arguments[0];
+  return arr.reduce((total, num) => {
+    return total * num;
+  }, 1);
 };
 
 const power = function() {
-	
+	let arr = arguments[0];
+  return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	let num = arguments[0];
+
+  if (num === 0) {
+    return 1;
+  }else if(num < 0) {
+    return -1;
+  } else {
+    return (num * factorial(num-1));
+  }
 };
 
 // Do not edit below this line
