179ec654ddd466d586d1f925aadf51b038ef47da
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-23 19:55:39 -0500

    Do 10 Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = function() {
+    let num = parseInt(arguments[0]);
+    if (num < 1) return 'OOPS'
+    let n1 = 0, n2 = 1, nextNum;
 
+    for (let i = 1; i <= num; i++) {
+        nextNum = n1 + n2;
+        n1 = n2;
+        n2 = nextNum;
+    }
+
+    return n1;
 };
 
 // Do not edit below this line
